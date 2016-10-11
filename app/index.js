1f525e8da5265f1d14394b82f625467289f0b0f6
charlier-shoe <charlier.shoe@gmail.com>
2016-10-12 00:00:04 +0900

    IDで指定したオブジェクトをDBから取得するAPIを追加

--- app/index.js
+++ app/index.js
@@ -1,10 +1,34 @@
-var express = require("express");
+var express = require('express');
+var bodyParser = require('body-parser');
+var oracledb = require('oracledb');
+
+var PORT = process.env.PORT || 8089;
+
 var app = express();
 
-var PORT = process.env.PORT || 80;
+var connectionProperties = {
+    user: process.env.DBAAS_USER_NAME || "oracle",
+    password: process.env.DBAAS_USER_PASSWORD || "password",
+    connectString: process.env.DBAAS_DEFAULT_CONNECT_DESCRIPTOR || "URL"
+};
 
-var server = app.listen(PORT, function() {
-    console.log("Node.js is listening to PORT:" + server.address().port);
+function doRelease(connection) {
+    connection.release(function (err) {
+        if (err) {
+            console.error(err.message);
+        }
+    });
+}
+
+var router = express.Router();
+router.use(function (request, response, next) {
+    console.log("REQUEST:" + request.method + "   " + request.url);
+    console.log("BODY:" + JSON.stringify(request.body));
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    response.setHeader('Access-Control-Allow-Credentials', true);
+    next();
 });
 
 // 地図上の物のサンプルデータ
@@ -39,8 +63,48 @@ var stuffList = [
     }
 ]
 
-app.get("/stuff/list", function(req, res, next){
-    res.json(stuffList);
+router.route('/list').get(function(request, response) {
+    response.json(stuffList);
 });
 
+/**
+ * GET /id
+ * Returns the detail of a staff
+ */
+router.route('/:id').get(function (request, response) {
+    console.log("GET STAFF BY ID:" + request.params.id);
+    oracledb.getConnection(connectionProperties, function (err, connection) {
+        if (err) {
+            console.error(err.message);
+            response.status(500).send("Error connecting to DB");
+            return;
+        }
+        var id = request.params.id;
+        connection.execute(
+            "SELECT ID, NAME FROM STAFFS WHERE ID = :id",
+            [id],
+            {outFormat: oracledb.OBJECT},
+            function (err, result) {
+                if (err) {
+                    console.error(err.message);
+                    response.status(500).send("Error getting data from DB");
+                    doRelease(connection);
+                    return;
+                }
+                console.log("RESULTSET:" + JSON.stringify(result));
+                if (result.rows.length === 1) {
+                    var staff = {id: result.rows[0].ID, name: result.rows[0].NAME, geo_location: result.rows[0].GEO_LOCATION};
+                    response.json(staff);
+                    doRelease(connection);
+                } else {
+                    response.end();
+                }
+            }
+        );
+    });
+});
 
+app.use('/stuffs', router);
+var server = app.listen(PORT, function() {
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
